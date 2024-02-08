b6cd8030b88c1518e85af0c5e7e5b61954a4493a
MarkTBSS <marktbss@gmail.com>
2024-02-08 15:14:49 +0700

    [MOD] Comment for best understanding

--- main.go
+++ main.go
@@ -10,28 +10,28 @@ func swap(x, y *int) {
 
 func main() {
 	a := new(int)
-	fmt.Println(a)  // 0xa0
-	fmt.Println(&a) // 0xa1
+	fmt.Println(&a) // 0xAddressA
+	fmt.Println(a)  // 0xPointerAddressA
 	fmt.Println(*a) // 0
 
 	*a = 5
-	fmt.Println(a)  // 0xa0
-	fmt.Println(&a) // 0xa1
+	fmt.Println(&a) // 0xAddressA
+	fmt.Println(a)  // 0xPointerAddressA
 	fmt.Println(*a) // 5
 
 	b := 10
+	fmt.Println(&b) // 0xAddressB
 	fmt.Println(b)  // 10
-	fmt.Println(&b) // 0xb1
 
 	*a = b
-	fmt.Println(a)  // 0xa0
-	fmt.Println(&a) // 0xa1
+	fmt.Println(&a) // 0xAddressA
+	fmt.Println(a)  // 0xPointerAddressA
 	fmt.Println(*a) // 10
 
 	b = 500
 	a = &b
-	fmt.Println(a)  // 0xb1
-	fmt.Println(&a) // 0xa1
+	fmt.Println(&a) // 0xAddressA
+	fmt.Println(a)  // 0xAddressB
 	fmt.Println(*a) // 500
 
 	x := 5
