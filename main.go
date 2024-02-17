5b7200f8b2082a5e0705b15f8637e176cb478b77
MarkTBSS <marktbss@gmail.com>
2024-02-17 17:16:31 +0700

    [MOD] z pointer added

--- main.go
+++ main.go
@@ -9,30 +9,39 @@ func swap(x, y *int) {
 }
 
 func main() {
-	a := new(int)
-	fmt.Println(&a) // 0xAddressA
-	fmt.Println(a)  // 0xPointerAddressA
-	fmt.Println(*a) // 0
+	z := new(int)
+	fmt.Println(&z) // 0xZ01
+	fmt.Println(z)  // 0xZ02
+	fmt.Println(*z) // 0
+	fmt.Println("=====")
 
+	a := new(int)
 	*a = 5
-	fmt.Println(&a) // 0xAddressA
-	fmt.Println(a)  // 0xPointerAddressA
+	fmt.Println(&a) // 0xA01
+	fmt.Println(a)  // 0xA02
 	fmt.Println(*a) // 5
+	fmt.Println("=====")
 
 	b := 10
-	fmt.Println(&b) // 0xAddressB
+	fmt.Println(&b) // 0xB01
 	fmt.Println(b)  // 10
+	//fmt.Println(*b) // error
+	fmt.Println("=====")
 
 	*a = b
-	fmt.Println(&a) // 0xAddressA
-	fmt.Println(a)  // 0xPointerAddressA
+	fmt.Println(&a) // 0xA01
+	fmt.Println(a)  // 0xA02
 	fmt.Println(*a) // 10
+	fmt.Println("=====")
 
 	b = 500
 	a = &b
-	fmt.Println(&a) // 0xAddressA
-	fmt.Println(a)  // 0xAddressB
+	fmt.Println(&b) // 0xB01
+	fmt.Println(b)  // 500
+	fmt.Println(&a) // 0xA01
+	fmt.Println(a)  // 0xB01
 	fmt.Println(*a) // 500
+	fmt.Println("=====")
 
 	x := 5
 	y := 10
